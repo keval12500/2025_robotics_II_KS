b836a6383c9e9d0b80efb7fa03b0c4c13bf91884
keval12500 <158621548+keval12500@users.noreply.github.com>
2025-03-17 15:17:41 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -6,228 +6,139 @@ import numpy as np
 import math
 
 ## Functions for quaternion and rotation matrix conversion
-## The code is adapted from the general_robotics_toolbox package
-## Code reference: https://github.com/rpiRobotics/rpi_general_robotics_toolbox_py
 def hat(k):
-    """
-    Returns a 3 x 3 cross product matrix for a 3 x 1 vector
-
-             [  0 -k3  k2]
-     khat =  [ k3   0 -k1]
-             [-k2  k1   0]
-
-    :type    k: numpy.array
-    :param   k: 3 x 1 vector
-    :rtype:  numpy.array
-    :return: the 3 x 3 cross product matrix
-    """
-
-    khat=np.zeros((3,3))
-    khat[0,1]=-k[2]
-    khat[0,2]=k[1]
-    khat[1,0]=k[2]
-    khat[1,2]=-k[0]
-    khat[2,0]=-k[1]
-    khat[2,1]=k[0]
+    khat = np.zeros((3,3))
+    khat[0,1] = -k[2]
+    khat[0,2] = k[1]
+    khat[1,0] = k[2]
+    khat[1,2] = -k[0]
+    khat[2,0] = -k[1]
+    khat[2,1] = k[0]
     return khat
 
 def q2R(q):
-    """
-    Converts a quaternion into a 3 x 3 rotation matrix according to the
-    Euler-Rodrigues formula.
-    
-    :type    q: numpy.array
-    :param   q: 4 x 1 vector representation of a quaternion q = [q0;qv]
-    :rtype:  numpy.array
-    :return: the 3x3 rotation matrix    
-    """
-    
     I = np.identity(3)
     qhat = hat(q[1:4])
     qhat2 = qhat.dot(qhat)
     return I + 2*q[0]*qhat + 2*qhat2
-######################
 
 def euler_from_quaternion(q):
-    w=q[0]
-    x=q[1]
-    y=q[2]
-    z=q[3]
-    # euler from quaternion
+    w, x, y, z = q
     roll = math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y))
     pitch = math.asin(2 * (w * y - z * x))
     yaw = math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z))
-
-    return [roll,pitch,yaw]
+    return [roll, pitch, yaw]
 
 class TrackingNode(Node):
     def __init__(self):
         super().__init__('tracking_node')
         self.get_logger().info('Tracking Node Started')
         
-        # Current object pose
         self.obs_pose = None
         self.goal_pose = None
         
-        # ROS parameters
         self.declare_parameter('world_frame_id', 'odom')
 
-        # Create a transform listener
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         
-        # Create publisher for the control command
         self.pub_control_cmd = self.create_publisher(Twist, '/track_cmd_vel', 10)
-        # Create a subscriber to the detected object pose
         self.sub_detected_goal_pose = self.create_subscription(PoseStamped, 'detected_color_object_pose', self.detected_obs_pose_callback, 10)
         self.sub_detected_obs_pose = self.create_subscription(PoseStamped, 'detected_color_goal_pose', self.detected_goal_pose_callback, 10)
 
-        # Create timer, running at 100Hz
         self.timer = self.create_timer(0.01, self.timer_update)
     
     def detected_obs_pose_callback(self, msg):
-        #self.get_logger().info('Received Detected Object Pose')
-        
         odom_id = self.get_parameter('world_frame_id').get_parameter_value().string_value
         center_points = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
         
-        # TODO: Filtering (labeled as optional?) 
-        # You can decide to filter the detected object pose here
-        # For example, you can filter the pose based on the distance from the camera
-        # or the height of the object
-        # if np.linalg.norm(center_points) > 3 or center_points[2] > 0.7:
-        #     return
-        
         try:
-            # Transform the center point from the camera frame to the world frame
-            transform = self.tf_buffer.lookup_transform(odom_id,msg.header.frame_id,rclpy.time.Time(),rclpy.duration.Duration(seconds=0.1))
-            t_R = q2R(np.array([transform.transform.rotation.w,transform.transform.rotation.x,transform.transform.rotation.y,transform.transform.rotation.z]))
-            cp_world = t_R@center_points+np.array([transform.transform.translation.x,transform.transform.translation.y,transform.transform.translation.z])
+            transform = self.tf_buffer.lookup_transform(odom_id, msg.header.frame_id, rclpy.time.Time(), rclpy.duration.Duration(seconds=0.1))
+            t_R = q2R(np.array([transform.transform.rotation.w, transform.transform.rotation.x, transform.transform.rotation.y, transform.transform.rotation.z]))
+            cp_world = t_R @ center_points + np.array([transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z])
         except TransformException as e:
-            self.get_logger().error('Transform Error: {}'.format(e))
+            self.get_logger().error(f'Transform Error: {e}')
             return
         
-        # Get the detected object pose in the world frame
         self.obs_pose = cp_world
 
     def detected_goal_pose_callback(self, msg):
-        #self.get_logger().info('Received Detected Object Pose')
-        
         odom_id = self.get_parameter('world_frame_id').get_parameter_value().string_value
         center_points = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
         
-        # TODO: Filtering
-        # You can decide to filter the detected object pose here
-        # For example, you can filter the pose based on the distance from the camera
-        # or the height of the object
-        # if np.linalg.norm(center_points) > 3 or center_points[2] > 0.7:
-        #     return
-        
         try:
-            # Transform the center point from the camera frame to the world frame
-            transform = self.tf_buffer.lookup_transform(odom_id,msg.header.frame_id,rclpy.time.Time(),rclpy.duration.Duration(seconds=0.1))
-            t_R = q2R(np.array([transform.transform.rotation.w,transform.transform.rotation.x,transform.transform.rotation.y,transform.transform.rotation.z]))
-            cp_world = t_R@center_points+np.array([transform.transform.translation.x,transform.transform.translation.y,transform.transform.translation.z])
+            transform = self.tf_buffer.lookup_transform(odom_id, msg.header.frame_id, rclpy.time.Time(), rclpy.duration.Duration(seconds=0.1))
+            t_R = q2R(np.array([transform.transform.rotation.w, transform.transform.rotation.x, transform.transform.rotation.y, transform.transform.rotation.z]))
+            cp_world = t_R @ center_points + np.array([transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z])
         except TransformException as e:
-            self.get_logger().error('Transform Error: {}'.format(e))
+            self.get_logger().error(f'Transform Error: {e}')
             return
         
-        # Get the detected object pose in the world frame
         self.goal_pose = cp_world
-        
+    
     def get_current_poses(self):
-        
         odom_id = self.get_parameter('world_frame_id').get_parameter_value().string_value
-        # Get the current robot pose
         try:
-            # from base_footprint to odom
             transform = self.tf_buffer.lookup_transform('base_footprint', odom_id, rclpy.time.Time())
             robot_world_x = transform.transform.translation.x
             robot_world_y = transform.transform.translation.y
             robot_world_z = transform.transform.translation.z
             robot_world_R = q2R([transform.transform.rotation.w, transform.transform.rotation.x, transform.transform.rotation.y, transform.transform.rotation.z])
-            obstacle_pose = robot_world_R@self.obs_pose+np.array([robot_world_x,robot_world_y,robot_world_z])
-            goal_pose = robot_world_R@self.goal_pose+np.array([robot_world_x,robot_world_y,robot_world_z])
-    
-        
+            obstacle_pose = robot_world_R @ self.obs_pose + np.array([robot_world_x, robot_world_y, robot_world_z])
+            goal_pose = robot_world_R @ self.goal_pose + np.array([robot_world_x, robot_world_y, robot_world_z])
         except TransformException as e:
-            self.get_logger().error('Transform error: ' + str(e))
-            return
+            self.get_logger().error(f'Transform Error: {e}')
+            return None, None
         
         return obstacle_pose, goal_pose
     
     def timer_update(self):
-    # Stop if the goal is not detected
-    if self.goal_pose is None:
-        cmd_vel = Twist()
-        cmd_vel.linear.x = 0.0
-        cmd_vel.angular.z = 0.0
+        if self.goal_pose is None:
+            cmd_vel = Twist()
+            self.pub_control_cmd.publish(cmd_vel)
+            return
+
+        current_obs_pose, current_goal_pose = self.get_current_poses()
+        if current_obs_pose is None or current_goal_pose is None:
+            return
+
+        goal_distance = np.linalg.norm(current_goal_pose[:2])
+        if goal_distance < 0.3:
+            cmd_vel = Twist()
+            self.pub_control_cmd.publish(cmd_vel)
+            return
+
+        cmd_vel = self.controller(current_obs_pose, current_goal_pose)
         self.pub_control_cmd.publish(cmd_vel)
-        return
 
-    # Get the current object pose in the robot base_footprint frame
-    current_obs_pose, current_goal_pose = self.get_current_poses()
-
-    # Calculate distance to goal
-    goal_distance = np.linalg.norm(current_goal_pose[:2])
-
-    # Stop if within 0.3 meters of the goal
-    if goal_distance < 0.3:
-        cmd_vel = Twist()
-        cmd_vel.linear.x = 0.0
-        cmd_vel.angular.z = 0.0
-        self.pub_control_cmd.publish(cmd_vel)
-        return
-
-    # Get the control velocity command
-    cmd_vel = self.controller(current_obs_pose, current_goal_pose)
-
-    # Publish the control command
-    self.pub_control_cmd.publish(cmd_vel)
-
-        #################################################
-    
     def controller(self, current_obs_pose, current_goal_pose):
-    cmd_vel = Twist()
+        cmd_vel = Twist()
+        safe_distance = 0.5
+        linear_speed = 0.2
+        angular_speed = 0.5
 
-    # Parameters for obstacle avoidance
-    safe_distance = 0.5  # Minimum distance to maintain from the obstacle
-    linear_speed = 0.2   # Base speed
-    angular_speed = 0.5  # Turning speed
+        goal_vector = np.array([current_goal_pose[0], current_goal_pose[1]])
+        if current_obs_pose is not None:
+            obs_vector = np.array([current_obs_pose[0], current_obs_pose[1]])
+            obs_distance = np.linalg.norm(obs_vector)
+        else:
+            obs_distance = float('inf')
 
-    # Vector to goal
-    goal_vector = np.array([current_goal_pose[0], current_goal_pose[1]])
+        if obs_distance < safe_distance:
+            angle_to_obstacle = math.atan2(obs_vector[1], obs_vector[0])
+            cmd_vel.angular.z = angular_speed if angle_to_obstacle < 0 else -angular_speed
+        else:
+            angle_to_goal = math.atan2(goal_vector[1], goal_vector[0])
+            cmd_vel.linear.x = linear_speed
+            cmd_vel.angular.z = angle_to_goal * 0.5
 
-    # Vector to obstacle
-    if current_obs_pose is not None:
-        obs_vector = np.array([current_obs_pose[0], current_obs_pose[1]])
-        obs_distance = np.linalg.norm(obs_vector)
-    else:
-        obs_distance = float('inf')  # No obstacle detected
-
-    # Collision avoidance logic
-    if obs_distance < safe_distance:
-        # If obstacle is too close, steer away
-        angle_to_obstacle = math.atan2(obs_vector[1], obs_vector[0])
-        cmd_vel.linear.x = 0.0
-        cmd_vel.angular.z = angular_speed if angle_to_obstacle < 0 else -angular_speed
-    else:
-        # Navigate toward the goal
-        angle_to_goal = math.atan2(goal_vector[1], goal_vector[0])
-        cmd_vel.linear.x = linear_speed
-        cmd_vel.angular.z = angle_to_goal * 0.5  # Smooth turning adjustment
-
-    return cmd_vel
+        return cmd_vel
 
 def main(args=None):
-    # Initialize the rclpy library
     rclpy.init(args=args)
-    # Create the node
     tracking_node = TrackingNode()
     rclpy.spin(tracking_node)
-    # Destroy the node explicitly
     tracking_node.destroy_node()
-    # Shutdown the ROS client library for Python
     rclpy.shutdown()
 
 if __name__ == '__main__':
